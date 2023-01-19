7befd082d385e211235fe51439f6fbcf771a53c0
athishanna@gmail.com <athishanna@gmail.com>
2023-01-19 17:16:23 +0530

    m

---
+++ Challenges/tree/welcome.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	printf("Congrats on getting this file hidden inside the directree\n");
+	printf("You just have to find flag from here on your own\n");
+	string s="ShaastraCTF{D!rectree_depth_2_wa5_9u!ck_t0_traver5e}";
+	printf("Happy Solving\n");
+}
